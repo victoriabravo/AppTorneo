278bdd0b12145bca17e36d02b936d8f15d5d7e26
victoria <victoriabravo994@gmail.com>
2021-03-16 09:54:29 -0300

    pantallas

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -23,6 +23,26 @@ var app = new Framework7({
         path: '/index/',
         url: 'index.html',
       },
+      {
+        path: '/equiposA1/',
+        url: 'equiposA1.html',
+      },
+      {
+        path: '/equiposA2/',
+        url: 'equiposA2.html',
+      },
+      {
+        path: '/fechas/',
+        url: 'fechas.html',
+      },
+      {
+        path: '/contacto/',
+        url: 'contacto.html',
+      },
+      {
+        path: '/noticias/',
+        url: 'noticias.html',
+      },
     ]  
     // ... other parameters
   });
@@ -44,6 +64,9 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
 
 console.log("entramos a index!")
 
+  
+   
+})
      $$('#btnIngresar').on('click', function(){
         mainView.router.navigate('/categorias/');
     })
@@ -51,11 +74,44 @@ console.log("entramos a index!")
     $$('#btnLoguear').on('click', cambiarForm);
     $$('#btnLogin').on('click', fnLogin);
     
-})
+
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="categorias"]', function (e) {
     
-    console.log("entramos a categorias!")
+    console.log("entramos a categorias!");
+    $$('#catA1').on('click', function(){
+      mainView.router.navigate('/equiposA1/');
+    })
+    $$('#catA2').on('click', function(){
+      mainView.router.navigate('/equiposA2/');
+    })
+
+})
+ 
+
+$$(document).on('page:init', '.page[data-name="equiposA1"]', function (e) {
+    
+    console.log("entramos a equiposA1!")
+    $$('#fechas').on('click', function(){
+      mainView.router.navigate('/fechas/');
+    })
+})
+
+$$(document).on('page:init', '.page[data-name="equiposA2"]', function (e) {
+    
+    console.log("entramos a equiposA2!")
+})
+$$(document).on('page:init', '.page[data-name="fechas"]', function (e) {
+    
+    console.log("entramos a fechas!")
+})
+$$(document).on('page:init', '.page[data-name="contacto"]', function (e) {
+    
+    console.log("entramos a contacto!")
+})
+$$(document).on('page:init', '.page[data-name="noticias"]', function (e) {
+    
+    console.log("entramos a noticias!")
 })
 
 function cambiarForm(){
@@ -65,7 +121,7 @@ function cambiarForm(){
     else{
         $$('#loginForm').removeClass('mostrar').addClass('ocultar')
     } 
-}
+};
 
 function fnLogin(){
     email = $$('#emailLogin').val();
@@ -82,4 +138,3 @@ function fnLogin(){
       });
 
 }
-
